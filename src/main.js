7f82f60eddc156632172bb533568a3ae1f96fbcf
PabloSanFrancisco <psanfrancisco@uc.cl>
2023-06-24 21:35:57 -0400

    Subir carpetas

--- src/main.js
+++
@@ -1,9 +0,0 @@
-fetch('./Dataset/draft2.csv') // Replace 'data.csv' with the actual filename if different
-  .then(response => response.text())
-  .then(csvData => processData(csvData));
-
-function processData(csv) {
-  // Parse CSV data here and perform desired operations
-  console.log(csv);
-  // Process the data further as needed
-}
