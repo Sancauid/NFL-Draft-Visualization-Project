4a59e72cf10633b91e4a338ec7728d11b1fe5540
PabloSanFrancisco <psanfrancisco@uc.cl>
2023-06-26 11:14:39 -0400

    Heatmap funcional

--- Visualization/main.js
+++ Visualization/main.js
@@ -1,7 +1,7 @@
 DRAFT_DATABASE = "https://gist.githubusercontent.com/kunafuego/106a696e84cbd056e4e2c6a2cc6e8387/raw/db12d21344c086a6e1c6a4e8b769e086e62043bf/draft.csv";
 KICKERS_DATABASE = "https://gist.githubusercontent.com/Sancauid/aee1813abb6fa4021006d6e5730ceac3/raw/74b87d4d8e9177b8987447d8009e089448c98e4c/kickers.csv";
 
-const WIDTH = 800;
+const WIDTH = 1200;
 const HEIGHT = 600;
 
 const svg = d3.select("#heatmap")
@@ -18,51 +18,107 @@ const MARGIN = {
 const HEIGHTVIS = HEIGHT - MARGIN.top - MARGIN.bottom;
 const WIDTHVIS = WIDTH - MARGIN.right - MARGIN.left;
 
-function heatMap(kickers) {
+function heatMapKickers(kickersPercentages) {
 
-  const escala_x = d3.scaleLinear()
-    .domain([0, 36000])
-    .range([MARGIN.left, WIDTH - MARGIN.right]);
+  let currentYearIndex = 0;
 
-  const escala_y = d3.scaleLinear()
-    .domain([0, 43000])
-    .range([HEIGHT - MARGIN.bottom, MARGIN.top]);
+  const svg = d3.select("#heatmap")
+      .append("svg")
+      .attr("width", WIDTH)
+      .attr("height", HEIGHT)
+      .append("g");
 
-  const ejeX = d3.axisBottom(escala_x);
-  const ejeY = d3.axisLeft(escala_y);
+  function updateHeatmap() {
+      const dataForYear = kickersPercentages[currentYearIndex].fieldGoalPercentages;
+    
+      svg.selectAll("rect").remove();
+      svg.selectAll("text").remove();
 
-  svg
-    .append("g")
-    .attr("transform", `translate(0, ${HEIGHTVIS + MARGIN.top})`)
-    .call(ejeX)
-    .selectAll("text")
-    .attr("font-size", 10);
+      const colorScale = d3.scaleSequential(d3.interpolateRdBu)
+      .domain([1, 0.2]);
 
-  svg
-    .append("g")
-    .attr("transform", `translate(${MARGIN.left}, 0)`)
-    .call(ejeY)
-    .selectAll("text")
-    .attr("font-size", 10);
+    svg.selectAll("rect")
+      .data(dataForYear, (d) => d.distance)
+      .enter()
+      .append("rect")
+      .attr("x", (_, i) => (i % 6) * (WIDTHVIS / 6) + MARGIN.left)
+      .attr("y", (_, i) => Math.floor(i / 6) * (HEIGHTVIS / Math.ceil(dataForYear.length / 6)) + MARGIN.top)
+      .attr("width", WIDTHVIS / 6)
+      .attr("height", HEIGHTVIS / Math.ceil(dataForYear.length / 6))
+      .attr("fill", (d) => colorScale(parseFloat(d.percentage)));
 
-  var colorScale = d3
-    .scaleLinear()
-    .domain([0, d3.max(kickers)])
-    .range(["blue", "red"]);
-  
+    svg.selectAll("text")
+      .data(dataForYear, (d) => d.distance)
+      .enter()
+      .append("text")
+      .attr("x", (_, i) => (i % 6) * (WIDTHVIS / 6) + MARGIN.left + (WIDTHVIS / 12))
+      .attr("y", (_, i) => Math.floor(i / 6) * (HEIGHTVIS / Math.ceil(dataForYear.length / 6)) + MARGIN.top * 9.5 + (HEIGHTVIS / (6 * Math.ceil(dataForYear.length / 6))))
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .text((d) => d.distance)
+      .style("fill", "black")
+      .style("font-size", "16px");
 
-  svg.append("g")
-    .attr("class", "heatmap")
-    .selectAll("rect")
-    .data(kickers)
-    .enter()
-    .append("rect")
-    .attr("x", d => escala_x(d))
-    .attr("y", d => escala_y(d))
-    .attr("width", WIDTH)
-    .attr("height", HEIGHT)
-    .style("fill", function(d) { return colorScale(d); });
+    svg.append("text")
+      .attr("x", WIDTH / 2)
+      .attr("y", MARGIN.top / 2)
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .text(kickersPercentages[currentYearIndex].year)
+      .style("fill", "black")
+      .style("font-size", "16px");
+
+    currentYearIndex = (currentYearIndex + 1) % kickersPercentages.length;
+  }
+
+  updateHeatmap();
+
+  setInterval(updateHeatmap, 500);
+    
+
+  const colorScale = d3.scaleSequential(d3.interpolateYlOrRd)
+      .domain([0, 1]);
+
+  svg.selectAll("rect")
+      .data(dataForYear)
+      .enter()
+      .append("rect")
+      .attr("x", (_, i) => (i % 6) * (WIDTH / 6))
+      .attr("y", (_, i) => Math.floor(i / 6) * (HEIGHT / Math.ceil(dataForYear.length / 6)))
+      .attr("width", WIDTH / 6)
+      .attr("height", HEIGHT)
+      .attr("fill", d => colorScale(parseFloat(d.percentage)));
 }
+
+function calculateFieldGoalPercentages(data) {
+  const results = data.map(entry => {
+    const year = entry.Year;
+
+    const fgDistances = [
+      { distance: "0-19", percentage: parseFloat(entry["FGM0-19"]) / parseFloat(entry["FGA0-19"]) },
+      { distance: "20-29", percentage: parseFloat(entry["FGM20-29"]) / parseFloat(entry["FGA20-29"]) },
+      { distance: "30-39", percentage: parseFloat(entry["FGM30-39"]) / parseFloat(entry["FGA30-39"]) },
+      { distance: "40-49", percentage: parseFloat(entry["FGM40-49"]) / parseFloat(entry["FGA40-49"]) },
+      { distance: "50+", percentage: parseFloat(entry["FGM50+"]) / parseFloat(entry["FGA50+"]) },
+      { distance: "Total", percentage: parseFloat(entry["FGMTot"]) / parseFloat(entry["FGATot"]) }
+    ];
+
+    return {
+      year: year,
+      fieldGoalPercentages: fgDistances,
+    };
+  });
+
+  return results;
+}
+
+d3.csv(KICKERS_DATABASE)
+    .then((kickers) => {
+    console.log(kickers);
+    const kickersPercentages = calculateFieldGoalPercentages(kickers);
+    heatMapKickers(kickersPercentages);
+  })
+  .catch((error) => console.log(error));
 
 d3.csv(DRAFT_DATABASE)
     .then((draft) => {
@@ -70,12 +126,5 @@ d3.csv(DRAFT_DATABASE)
   })
   .catch((error) => console.log(error)); 
 
-d3.csv(KICKERS_DATABASE)
-    .then((kickers) => {
-    console.log("Kickers");
-    console.log(kickers);
-    heatMap(kickers);
-  })
-  .catch((error) => console.log(error));
 
 
